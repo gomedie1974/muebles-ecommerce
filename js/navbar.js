405e61500dc0a8497340fe5167ca6c6e7b67bf02
Diego Gomez <gomedie1974@hotmail.com>
2026-02-19 20:18:34 -0300

    modificando imagen carro

--- js/navbar.js
+++ js/navbar.js
@@ -48,16 +48,12 @@ function cargarNavbar() {
             <div id="usuario-area"></div>
 
             <!-- Carrito -->
-            <a href="carrito.html"
-               class="btn btn-outline-dark position-relative">
-
-              <i class="bi bi-cart"></i>
-
+            <a href="carrito.html" class="nav-link position-relative">
+              <i class="bi bi-cart3 fs-4"></i>
               <span id="contador-carrito"
-                class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-dark">
+                    class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
                 0
               </span>
-
             </a>
 
           </div>
