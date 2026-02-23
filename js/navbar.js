9dda789d7433b240830b066670a6d252342200cd
Diego Gomez <gomedie1974@hotmail.com>
2026-02-23 18:12:19 -0300

    agregando tienda

--- js/navbar.js
+++ js/navbar.js
@@ -49,11 +49,11 @@ function cargarNavbar() {
 
             <!-- Carrito -->
             <a href="carrito.html" class="nav-link position-relative">
-              <i class="bi bi-cart4 fs-4"></i>
+              <i class="bi bi-cart4 fs-4" ></i>
               <span id="contador-carrito"
                     class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
                 0
-              </span>
+              </span> 
             </a>
 
           </div>
